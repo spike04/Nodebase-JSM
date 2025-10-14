8ca8b64f38356297daae7c4da163eded687d49fe
spike04 <rubinbajracharya@yahoo.com>
2025-10-14 07:18:54 +0545

    chor: initial project setup with shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline">Click Me</Button>
+    </div>
+  )
+}
